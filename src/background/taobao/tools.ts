1dbf91bcfb217375fa3a6abdbacb2d4fb4ccbbe0
欧顶银 <oudingyin@oudingyindeMacBook-Pro.local>
2020-01-05 18:43:41 +0800

    .

--- src/background/taobao/tools.ts
+++ src/background/taobao/tools.ts
@@ -20,7 +20,7 @@ export function getToken() {
 }
 
 export async function getUserName() {
-  return decodeURIComponent((await getCookie("dnk")) || (await getCookie("__nk__")));
+  return eval(`"${(await getCookie("dnk")) || (await getCookie("__nk__"))}"`);
 }
 
 const appKey = "12574478";
