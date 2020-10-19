202d325276a51b089c6c480c409381947fedfd18
yuanxiaowa <yuanxiaowa@qq.com>
2020-10-19 23:50:08 +0800

    .

--- public/panel.js
+++ public/panel.js
@@ -1,41 +1,47 @@
-function handler() {
-  var list = document.querySelectorAll(".address-list>div");
-  var b = false;
-  var currentPrice = +document.querySelector(".realpay--price").textContent;
-  function exchangeAddress() {
-    if (b) {
-      list[0].click();
-    } else {
-      list[1].click();
+function getScript() {
+  function handler() {
+    var list = document.querySelectorAll(".address-list>div");
+    var b = false;
+    var currentPrice = +document.querySelector(".realpay--price").textContent;
+    function exchangeAddress() {
+      if (b) {
+        list[0].click();
+      } else {
+        list[1].click();
+      }
+      b = !b;
     }
-    b = !b;
-  }
-  const btn = document.querySelector("#submitOrderPC_1 a:last-child");
-  function submit() {
-    btn.click();
-  }
+    const btn = document.querySelector("#submitOrderPC_1 a:last-child");
+    function submit() {
+      btn.click();
+    }
 
-  send = XMLHttpRequest.prototype.send;
-  XMLHttpRequest.prototype.send = function(data) {
-    if (this.__sufei_url.startsWith("/auction/json/async_linkage.do?")) {
-      const listener = () => {
-        const { data } = JSON.parse(this.responseText);
-        if (+data.realPayPC_1.fields.price < currentPrice) {
-          submit();
-        } else {
-          exchangeAddress();
-        }
-        this.removeEventListener("load", listener);
-      };
-      this.addEventListener("load", listener);
-    }
-    return send.call(this, data);
-  };
-  exchangeAddress();
+    send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(data) {
+      if (this.__sufei_url.startsWith("/auction/json/async_linkage.do?")) {
+        const listener = () => {
+          const { data } = JSON.parse(this.responseText);
+          if (+data.realPayPC_1.fields.price < currentPrice) {
+            submit();
+          } else {
+            exchangeAddress();
+          }
+          this.removeEventListener("load", listener);
+        };
+        this.addEventListener("load", listener);
+      }
+      return send.call(this, data);
+    };
+    exchangeAddress();
+  }
+  var ele = document.createElement("script");
+  ele.textContent = `(${handler.toString()})()`;
+  document.body.appendChild(ele);
+  document.body.removeChild(ele);
 }
 
 document.querySelector("button").onclick = () => {
   chrome.tabs.executeScript({
-    code: `console.log(\`(${handler.toString().replace(/`/g, '\\`')})()\`)`,
+    code: `(${getScript.toString()})()`,
   });
 };
